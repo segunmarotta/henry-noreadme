3dd2118511a7204e42c9bfc0532b964a064bdb58
SegundoMarotta <segunmarotta@hotmail.com>
2023-04-10 20:47:39 -0300

    bucles

---
+++ Carpeta1/Bucles.js
@@ -0,0 +1,18 @@
+var suma = 0;
+
+
+//for (let i = 0; i < 5; i++) {
+//   suma = suma + i;
+//    console.log("variable de interacion", i);  
+//}
+//console.log("variable suma", suma);
+
+
+while (suma < 3) {
+    suma = suma + 1;
+    console.log(suma);
+    
+}
+
+//FOR se usa cuando sabemos la cantidad de pasos que queremos ejecutar
+//WHILE no sabemos con certeza cuantos pasos necesitaremos hasta finalizar la ejecucion
